9130a12b68c54ea7e3a88c4080fb13349f650e3f
Rui Malheiro <rmpmalheiro@gmail.com>
2026-05-19 11:59:07 +0100

    import kv_cache directly in model implementation

--- models/implementations/tendril.py
+++ models/implementations/tendril.py
@@ -3,7 +3,7 @@ import torch.nn.functional as F
 
 from torch import nn
 from models.base import BaseModel
-from inference import KVCache
+from inference.kv_cache import KVCache
 from config import ModelConfig
 
 
