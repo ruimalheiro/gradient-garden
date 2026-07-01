5fd62f564c6fd7b529fef17705cfab8671813d50
Rui Malheiro <rmpmalheiro@gmail.com>
2026-07-01 19:59:32 +0100

    Fix bug in evaluate (was using self....)

--- evals/evaluation.py
+++ evals/evaluation.py
@@ -23,7 +23,7 @@ def evaluate_hellaswag(
 ):
     return evaluate_multiple_choice_task(
         task=EvalTask.HELLASWAG,
-        filepath=f'{config.paths.evals.hellaswag_path}/{self.config.paths.evals.data_filename}',
+        filepath=f'{config.paths.evals.hellaswag_path}/{config.paths.evals.data_filename}',
         inference_runtime=inference_runtime,
         batch_size=batch_size,
         num_examples=num_examples
@@ -37,7 +37,7 @@ def evaluate_winogrande(
 ):
     return evaluate_multiple_choice_task(
         task=EvalTask.WINOGRANDE,
-        filepath=f'{config.paths.evals.winogrande_path}/{self.config.paths.evals.data_filename}',
+        filepath=f'{config.paths.evals.winogrande_path}/{config.paths.evals.data_filename}',
         inference_runtime=inference_runtime,
         batch_size=batch_size,
         num_examples=num_examples
@@ -51,7 +51,7 @@ def evaluate_arc_challenge(
 ):
     return evaluate_multiple_choice_task(
         task=EvalTask.ARC_CHALLENGE,
-        filepath=f'{config.paths.evals.arc_challenge_path}/{self.config.paths.evals.data_filename}',
+        filepath=f'{config.paths.evals.arc_challenge_path}/{config.paths.evals.data_filename}',
         inference_runtime=inference_runtime,
         batch_size=batch_size,
         num_examples=num_examples
@@ -77,6 +77,7 @@ def evaluate_multiple_choice_task(
 ):
     model = inference_runtime.model
     device = inference_runtime.device
+    tokenizer= inference_runtime.tokenizer
 
     model.eval()
 
@@ -86,7 +87,8 @@ def evaluate_multiple_choice_task(
         filepath=filepath,
         ddp=False,
         is_master_process=True,
-        size=size,
+        pad_token_id=tokenizer.pad_id,
+        size=size
     )
 
     num_correct_norm = 0
