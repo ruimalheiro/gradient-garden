7eb61744a9fe78ad8dd81745bc9590147e0072b2
Rui Malheiro <rmpmalheiro@gmail.com>
2026-09-02 13:31:24 +0100

    loading parquet from cursor helper

--- datasets_preparation/utils/parquet_search.py
+++ datasets_preparation/utils/parquet_search.py
@@ -3,28 +3,29 @@ import pyarrow.parquet as pq
 from huggingface_hub import HfApi, HfFileSystem
 from concurrent.futures import ThreadPoolExecutor
 from bisect import bisect_right
+from datasets import load_dataset
 
 
 def find_parquet_files(
     *,
-    dataset_id,
+    ds_id,
     revision,
     token
 ):
     api = HfApi(token=token)
 
-    files = api.list_repo_files(dataset_id, repo_type='dataset', revision=revision)
+    files = api.list_repo_files(ds_id, repo_type='dataset', revision=revision)
 
     parquet_files = sorted(path for path in files if path.endswith('.parquet'))
 
     if not parquet_files:
-        raise ValueError(f'No parquet files found for {dataset_id}@{revision}')
+        raise ValueError(f'No parquet files found for {ds_id}@{revision}')
 
     return parquet_files
 
 def get_parquet_row_counts(
     *,
-    dataset_id,
+    ds_id,
     revision,
     files,
     token,
@@ -33,7 +34,7 @@ def get_parquet_row_counts(
     fs = HfFileSystem(token=token)
 
     def count_rows(path):
-        with fs.open(f'datasets/{dataset_id}@{revision}/{path}', 'rb') as f:
+        with fs.open(f'datasets/{ds_id}@{revision}/{path}', 'rb') as f:
             return pq.ParquetFile(f).metadata.num_rows
 
     with ThreadPoolExecutor(max_workers=num_proc) as pool:
@@ -86,3 +87,42 @@ def find_document_cursor(index, offset):
         'next_file': file['path'],
         'next_row': offset - file['start_document']
     }
+
+def load_parquet_from_cursor(
+    *,
+    ds_id,
+    revision,
+    split,
+    index,
+    cursor,
+    token
+):
+    next_file = cursor['next_file']
+    if next_file is None:
+        raise ValueError('Cannot load cursor as it points to the end of the file')
+
+    files = [file['path'] for file in index['files']]
+
+    try:
+        file_index = files.index(next_file)
+    except ValueError:
+        raise ValueError(f'The cursor file: {next_file!r} was not found in the parquet index')
+
+    remaining_files = files[file_index:]
+
+    data_files = { split: [f'hf://datasets/{ds_id}@{revision}/{path}' for path in remaining_files] }
+
+    ds = load_dataset(
+        'parquet',
+        data_files=data_files,
+        split=split,
+        streaming=True,
+        token=token
+    )
+
+    next_row = cursor['next_row']
+
+    if next_row > 0:
+        ds = ds.skip(next_row)
+
+    return ds
