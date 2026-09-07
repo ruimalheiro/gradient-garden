7021edaf46cf3d4e60a2810eb6ca4f102ce42eff
Rui Malheiro <rmpmalheiro@gmail.com>
2026-09-07 13:49:49 +0100

    revert changes

--- datasets_preparation/prepare_pretraining_dataset.py
+++ datasets_preparation/prepare_pretraining_dataset.py
@@ -97,7 +97,6 @@ def download_and_prepare_data(
 ):
     prepared_datasets = []
     source_metadata = {}
-    source_cursor = None
     for dataset in valid_datasets:
         ds_id = dataset['id']
         name = dataset.get('name', None)
@@ -136,7 +135,7 @@ def download_and_prepare_data(
         if search_parquet is True:
             logger.info(f'The "search_parquet" flag is set. Using parquet loader...')
 
-            ds, source_cursor = load_dataset_with_search_parquet(
+            ds = load_dataset_with_search_parquet(
                 ds_id=ds_id,
                 split=split,
                 streaming=True,
@@ -203,7 +202,7 @@ def download_and_prepare_data(
     else:
         prepared_dataset = prepared_datasets[0]
 
-    return prepared_dataset, source_metadata, source_cursor
+    return prepared_dataset, source_metadata
 
 tokenizer = None
 def tokenize(tokenizer_kwargs, doc):
@@ -244,10 +243,7 @@ def prepare_pretraining_dataset(
     if not 0.0 < validation_ratio < 1.0:
         raise ValueError('"validation_ratio" must be > 0 and < 1')
 
-    if any(dataset.get('transforms', {}).get('search_parquet', False) for dataset in valid_datasets) and len(valid_datasets) > 1:
-        raise ValueError('The "search_parquet" transform is currently supported only for single source dataset preparation.')
-
-    prepared_dataset, source_metadata, source_cursor = download_and_prepare_data(
+    prepared_dataset, source_metadata = download_and_prepare_data(
         config=config,
         seed=seed,
         valid_datasets=valid_datasets,
@@ -276,6 +272,5 @@ def prepare_pretraining_dataset(
         validation_ratio=validation_ratio,
         num_proc=num_proc,
         chunksize=config.data_preparation.mp_pool_chunk_size,
-        source_metadata=source_metadata,
-        source_cursor=source_cursor
+        source_metadata=source_metadata
     )
