73a0a56d9d41729f0cb1f9410dafd1108eb674a2
Rui Malheiro <rmpmalheiro@gmail.com>
2026-09-23 12:52:04 +0000

    removing guard as now we support multiple source plus continuation

--- datasets_preparation/prepare_pretraining_dataset.py
+++ datasets_preparation/prepare_pretraining_dataset.py
@@ -251,10 +251,6 @@ def prepare_pretraining_dataset(
     #### VERIFY MIX FILE STRUCTURE
     seed, common_settings, valid_datasets, probabilities = assert_common_structure_and_extract(datasets_mix, SUPPORTED_HF_DATASETS)
 
-    offset_datasets = [dataset for dataset in valid_datasets if int(dataset.get('transforms', {}).get('start_document', 0)) > 0]
-    if offset_datasets and len(valid_datasets) != 1:
-        raise ValueError('start_document currently supports only single-source dataset preparation')
-
     train_mix_target_tokens = common_settings.get('target_tokens')
     if train_mix_target_tokens is not None:
         train_mix_target_tokens = int(train_mix_target_tokens)
